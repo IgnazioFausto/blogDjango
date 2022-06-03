380449394ea7a88303359a6c6287d32137ba6a0c
IgnazioFausto <nachojam777@gmail.com>
2022-06-03 13:00:22 +0300

    heroku

--- blogCoder/blogCoder/settings.py
+++ blogCoder/blogCoder/settings.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 import os
-
+import dj_database_url;
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +80,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 # }
 # DATABASES['default'].update(db_from_env)
 
-from dj_database_url import *
+
 from decouple import config
 
 DATABASES = {
