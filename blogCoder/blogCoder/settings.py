932e9a750d4edb06d2062b29cac27b1ae217ad42
IgnazioFausto <nachojam777@gmail.com>
2022-06-03 12:56:16 +0300

    herok

--- blogCoder/blogCoder/settings.py
+++ blogCoder/blogCoder/settings.py
@@ -80,7 +80,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 # }
 # DATABASES['default'].update(db_from_env)
 
-import dj_database_url;
+from dj_database_url import *
 from decouple import config
 
 DATABASES = {
