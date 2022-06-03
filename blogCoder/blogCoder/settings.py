63d8c83f448365f0eae9665bf3c40df9414762f4
IgnazioFausto <nachojam777@gmail.com>
2022-06-03 11:36:19 +0300

    heroku

--- blogCoder/blogCoder/settings.py
+++ blogCoder/blogCoder/settings.py
@@ -185,9 +185,7 @@ DATABASES['default'].update(db_from_env)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (  
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
